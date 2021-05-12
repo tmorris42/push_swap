03928880698bcc3b41d2a653666a5b0f3cc84c0b
tmorris <tmorris@student.42.fr>
2021-05-12 11:26:07 +0200

    Update tests

--- test.py
+++ test.py
@@ -6,13 +6,14 @@ ERROR = -1
 KO = 0
 OK = 1
 
-def check_nums(nums):
-    out_bytes = subprocess.check_output(f"./push_swap {nums} | wc -l", shell=True)
+def check_nums(program_name, nums):
+    out_bytes = subprocess.check_output(f"{program_name} {nums} | wc -l", shell=True)
     number_of_lines = int(out_bytes.decode("utf-8"))
-    out_bytes = subprocess.check_output(f"./push_swap {nums} | ./checker {nums}", shell=True)
+    out_bytes = subprocess.check_output(f"{program_name} {nums} | ./checker {nums}", shell=True)
     out_str = out_bytes.decode("utf-8")
-    print(f"Testing {nums}\t\tOperations Needed: {number_of_lines}", end=": ")
+    print(f"({number_of_lines})\tTest [{nums}]", end="\t")
     if "Error\n" in out_str:
+        #print("ERROR MESSAGES: ==========\n", out_str, "\n==========\n")
         return -1
     if "KO\n" in out_str:
         return 0
@@ -20,19 +21,9 @@ def check_nums(nums):
         return 1
     return -2
 
-def check_error(nums):
-    if check_nums(nums) == -1:
-        return 1
-    return 0
-
-def check_KO(nums):
-    if check_nums(nums) == 0:
-        return 1
-    return 0
-
-def check_OK(nums, expected=1):
+def check_OK(program_name, nums, expected=1):
     msg = ["ERROR", "KO", "OK"]
-    res = check_nums(nums)
+    res = check_nums(program_name, nums)
     if res == expected:
         print("\033[0;32m", end="")
     else:
@@ -42,21 +33,29 @@ def check_OK(nums, expected=1):
 
 if __name__ == '__main__':
     subprocess.call("make")
-    total = 0
-    success = 0
 
-    test_cases = ["4 2 3", "3 2 1 0", "-5 10 8 -7 94 -800 33"]
+    test_cases = [
+            "4 2 3",
+            "4 3 1",
+            "2 -1 5",
+            "-9 9 5",
+            "3 9 0",
+            "3 2 1 0",
+            "5 2 3 1 4",
+            "4 2 5 3 1",
+            "-5 10 8 -7 94 -800 33"]
     error_cases = ["1 one 3", "1 3 1 2", "2 % 4", "1 0 -5 7 -0 8",
             "2147483648", "-2147483649"]
-    ko_cases = []
-    for case in test_cases:
-        total += 1
-        success += check_OK(case, OK)
-    for case in error_cases:
-        total += 1
-        success += check_OK(case, ERROR)
-    for case in ko_cases:
-        total += 1
-        success += check_OK(case, KO)
+    program_names = ["./push_swap", "./sort_5", "./rough_sort"]
 
-    print(f"Tests Passed: {success}/{total}")
+    for program_name in program_names:
+        total = 0
+        success = 0
+        print(f"Checking {program_name}")
+        for case in test_cases:
+            total += 1
+            success += check_OK(program_name, case, OK)
+        for case in error_cases:
+            total += 1
+            success += check_OK(program_name, case, ERROR)
+        print(f"Tests Passed: {success}/{total}\n\n")
