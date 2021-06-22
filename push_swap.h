9b2a02346725c77e6e99f24b46c08ff156ee1b18
tmorris <tmorris@student.42.fr>
2021-06-22 18:13:39 +0200

    Create take_highest.c, Partially Normed

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 14:03:46 by tmorris           #+#    #+#             */
-/*   Updated: 2021/06/22 18:02:18 by tmorris          ###   ########.fr       */
+/*   Updated: 2021/06/22 18:12:29 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,16 @@ void	lowhigh_next_x(t_stack *stack, int x, int pivot, int *lowhigh);
 unsigned int	pass_lowest_x_rev(t_stack **a, t_stack **b, unsigned int x);
 unsigned int	pass_lowest_x_double(t_stack **a, t_stack **b, unsigned int x);
 unsigned int	pass_lowest_x(t_stack **a, t_stack **b, unsigned int x);
+int	take_highest_6_rev(t_stack **a, t_stack **b, int x);
+int	take_highest_x_rev(t_stack **a, t_stack **b, int x);
+int	take_highest_6(t_stack **a, t_stack **b, int x);
+int	take_highest_x(t_stack **a, t_stack **b, int x);
+void	insert_in_place(t_stack **a, t_stack**b);
+int	quicksort_left(t_stack **a, t_stack **b, int amount);
+void	rotate_high_to_bottom(t_stack **a);
+void	sort_top_3(t_stack **a, t_stack **b, int amount);
+int	sort_3(t_stack **a, t_stack **b);
+int	sort_5(t_stack **a, t_stack **b);
+void put_top_3(t_stack **a, t_stack **b, int amount);
 
 #endif
