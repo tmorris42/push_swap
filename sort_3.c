96bbc655cb76e6738935b44f7034c4180174e0b0
tmorris <tmorris@student.42.fr>
2021-06-23 15:31:47 +0200

    Partial Norm: sort_3.c

--- sort_3.c
+++ sort_3.c
@@ -31,61 +31,35 @@ void	put_top_3_rev(t_stack **a, t_stack **b, int amount)
 
 	if (amount < -3 || amount > -1)
 		return ;
-	first = stack_last(*b)->value;
 	if (amount == -1)
 	{
 		send_command("rrb", a, b);
 		send_command("pa", a, b);
 		return ;
 	}
-	second = stack_last(*b)->prev->value;
 	if (amount == -2)
 	{
-		send_command("rrb", a, b);
-		if (first > second)
-			send_command("pa", a, b);
-		send_command("rrb", a, b);
-		send_command("pa", a, b);
-		if (first < second)
-			send_command("pa", a, b);
+		if (stack_last(*b)->value > stack_last(*b)->prev->value)
+			send_command_chain("rrb pa rrb pa", a, b);
+		else
+			send_command_chain("rrb rrb pa pa", a, b);
 		return ;
 	}
+	first = stack_last(*b)->value;
+	second = stack_last(*b)->prev->value;
 	third = stack_last(*b)->prev->prev->value;
-	if (first > second && first > third)
-	{
-		send_command("rrb", a, b);
-		send_command("pa", a, b);
-		send_command("rrb", a, b);
-		if (second > third)
-			send_command("pa", a, b);
-		send_command("rrb", a, b);
-		send_command("pa", a, b);
-		if (second < third)
-			send_command("pa", a, b);
-	}
-	if (second > first && second > third)
-	{
-		send_command("rrb", a, b);
-		send_command("rrb", a, b);
-		send_command("pa", a, b);
-		if (first > third)
-			send_command("pa", a, b);
-		send_command("rrb", a, b);
-		send_command("pa", a, b);
-		if (first < third)
-			send_command("pa", a, b);
-	}
-	if (third > second && third > first)
-	{
-		send_command("rrb", a, b);
-		send_command("rrb", a, b);
-		send_command("rrb", a, b);
-		send_command("pa", a, b);
-		if (first > second)
-			send_command("sb", a, b);
-		send_command("pa", a, b);
-		send_command("pa", a, b);
-	}
+	if (first > second && first > third && second > third)
+		send_command_chain("rrb pa rrb pa rrb pa", a, b);
+	else if (first > second && first > third)
+			send_command_chain("rrb pa rrb rrb pa pa", a, b);
+	if (second > first && second > third && first > third)
+			send_command_chain("rrb rrb pa pa rrb pa", a, b);
+	else if (second > first && second > third)
+			send_command_chain("rrb rrb pa rrb pa pa", a, b);
+	if (third > second && third > first && first > second)
+		send_command_chain("rrb rrb rrb pa sb pa pa", a, b);
+	else if (third > second && third > first)
+		send_command_chain("rrb rrb rrb pa pa pa", a, b);
 }
 
 void	put_top_3(t_stack **a, t_stack **b, int amount)
@@ -111,17 +85,17 @@ void	put_top_3(t_stack **a, t_stack **b, int amount)
 	first = (*b)->value;
 	second = (*b)->next->value;
 	third = (*b)->next->next->value;
-	if (first < second && second < third) //1 2 3
+	if (first < second && second < third)
 		send_command_chain("sb pa sb pa sa pa", a, b);
-	else if (second < first && first < third) // 2 1 3
+	else if (second < first && first < third)
 		send_command_chain("pa sb pa sa pa", a, b);
-	else if (second < third && third < first) // 3 1 2
+	else if (second < third && third < first)
 		send_command_chain("pa sb pa pa", a, b);
-	else if (first < third && third < second) // 1 3 2
+	else if (first < third && third < second)
 		send_command_chain("rb pa pa rrb pa", a, b);
-	else if (third < first && first < second) // 2 3 1
+	else if (third < first && first < second)
 		send_command_chain("sb pa pa pa", a, b);
-	else if (third < second && second < first) // 3 2 1
+	else if (third < second && second < first)
 		send_command_chain("pa pa pa", a, b);
 }
 
