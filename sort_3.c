cae5e9df4f68c002fd2da74da0386ca9761c2984
tmorris <tmorris@student.42.fr>
2021-06-23 15:19:04 +0200

    Partial Norm: sort_3.c

--- sort_3.c
+++ sort_3.c
@@ -99,34 +99,30 @@ void	put_top_3(t_stack **a, t_stack **b, int amount)
 	if (amount < -3 || amount < -stack_len(*b))
 		return ;
 	if (amount < 0)
-	{
 		put_top_3_rev(a, b, amount);
-		return ;
-	}
 	if (amount == 2 && (*b)->next->value > (*b)->value)
 		send_command("sb", a, b);
 	if (amount == 2)
 		send_command("pa", a, b);
 	if (amount == 1 || amount == 2)
 		send_command("pa", a, b);
-	else if (amount == 3)
-	{
-		first = (*b)->value;
-		second = (*b)->next->value;
-		third = (*b)->next->next->value;
-		if (first < second && second < third) //1 2 3
-			send_command_chain("sb pa sb pa sa pa", a, b);
-		else if (second < first && first < third) // 2 1 3
-			send_command_chain("pa sb pa sa pa", a, b);
-		else if (second < third && third < first) // 3 1 2
-			send_command_chain("pa sb pa pa", a, b);
-		else if (first < third && third < second) // 1 3 2
-			send_command_chain("rb pa pa rrb pa", a, b);
-		else if (third < first && first < second) // 2 3 1
-			send_command_chain("sb pa pa pa", a, b);
-		else if (third < second && second < first) // 3 2 1
-			send_command_chain("pa pa pa", a, b);
-	}
+	if (amount != 3)
+		return ;
+	first = (*b)->value;
+	second = (*b)->next->value;
+	third = (*b)->next->next->value;
+	if (first < second && second < third) //1 2 3
+		send_command_chain("sb pa sb pa sa pa", a, b);
+	else if (second < first && first < third) // 2 1 3
+		send_command_chain("pa sb pa sa pa", a, b);
+	else if (second < third && third < first) // 3 1 2
+		send_command_chain("pa sb pa pa", a, b);
+	else if (first < third && third < second) // 1 3 2
+		send_command_chain("rb pa pa rrb pa", a, b);
+	else if (third < first && first < second) // 2 3 1
+		send_command_chain("sb pa pa pa", a, b);
+	else if (third < second && second < first) // 3 2 1
+		send_command_chain("pa pa pa", a, b);
 }
 
 void	sort_top_3_rev(t_stack **a, t_stack **b, int amount)
