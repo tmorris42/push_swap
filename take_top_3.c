8fd110dc5d18ac359881565f70b2781ad4dff39a
tmorris <tmorris@student.42.fr>
2021-06-23 15:41:49 +0200

    Norm take_top_3.c

--- take_top_3.c
+++ take_top_3.c
@@ -1,27 +1,11 @@
 #include "push_swap.h"
 
-void	put_top_3_rev(t_stack **a, t_stack **b, int amount)
+static void	run_top_3_rev_patterns(t_stack **a, t_stack **b)
 {
 	int	first;
 	int	second;
 	int	third;
 
-	if (amount < -3 || amount > -1)
-		return ;
-	if (amount == -1)
-	{
-		send_command("rrb", a, b);
-		send_command("pa", a, b);
-		return ;
-	}
-	if (amount == -2)
-	{
-		if (stack_last(*b)->value > stack_last(*b)->prev->value)
-			send_command_chain("rrb pa rrb pa", a, b);
-		else
-			send_command_chain("rrb rrb pa pa", a, b);
-		return ;
-	}
 	first = stack_last(*b)->value;
 	second = stack_last(*b)->prev->value;
 	third = stack_last(*b)->prev->prev->value;
@@ -39,26 +23,33 @@ void	put_top_3_rev(t_stack **a, t_stack **b, int amount)
 		send_command_chain("rrb rrb rrb pa pa pa", a, b);
 }
 
-void	put_top_3(t_stack **a, t_stack **b, int amount)
+void	put_top_3_rev(t_stack **a, t_stack **b, int amount)
+{
+	if (amount < -3 || amount > -1)
+		return ;
+	if (amount == -1)
+	{
+		send_command("rrb", a, b);
+		send_command("pa", a, b);
+		return ;
+	}
+	if (amount == -2)
+	{
+		if (stack_last(*b)->value > stack_last(*b)->prev->value)
+			send_command_chain("rrb pa rrb pa", a, b);
+		else
+			send_command_chain("rrb rrb pa pa", a, b);
+		return ;
+	}
+	run_top_3_rev_patterns(a, b);
+}
+
+static void	run_top_3_patterns(t_stack **a, t_stack **b)
 {
 	int	first;
 	int	second;
 	int	third;
 
-	if (!a || !b || !(*b) || amount > 3 || amount > stack_len(*b))
-		return ;
-	if (amount < -3 || amount < -stack_len(*b))
-		return ;
-	if (amount < 0)
-		put_top_3_rev(a, b, amount);
-	if (amount == 2 && (*b)->next->value > (*b)->value)
-		send_command("sb", a, b);
-	if (amount == 2)
-		send_command("pa", a, b);
-	if (amount == 1 || amount == 2)
-		send_command("pa", a, b);
-	if (amount != 3)
-		return ;
 	first = (*b)->value;
 	second = (*b)->next->value;
 	third = (*b)->next->next->value;
@@ -74,4 +65,22 @@ void	put_top_3(t_stack **a, t_stack **b, int amount)
 		send_command_chain("sb pa pa pa", a, b);
 	else if (third < second && second < first)
 		send_command_chain("pa pa pa", a, b);
+}
+
+void	put_top_3(t_stack **a, t_stack **b, int amount)
+{
+	if (!a || !b || !(*b) || amount > 3 || amount > stack_len(*b))
+		return ;
+	if (amount < -3 || amount < -stack_len(*b))
+		return ;
+	if (amount < 0)
+		put_top_3_rev(a, b, amount);
+	if (amount == 2 && (*b)->next->value > (*b)->value)
+		send_command("sb", a, b);
+	if (amount == 2)
+		send_command("pa", a, b);
+	if (amount == 1 || amount == 2)
+		send_command("pa", a, b);
+	if (amount == 3)
+		run_top_3_patterns(a, b);
 }
